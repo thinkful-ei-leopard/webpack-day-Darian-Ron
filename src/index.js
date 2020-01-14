52a4070a151794e9231c04489b95574285b5b3bc
Ron Martin <ronmartin89@gmail.com>
2020-01-14 12:57:28 -0800

    Our app is working! Refactored with npm and webpack, all code is wired together correctly.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import $ from 'jquery';
-import cuid from 'cuid';
 import shoppingList from './shopping-list';
-import store from './store';
-import item from './item';
 
 import './index.css';
+
 
 const main = function () {
   shoppingList.bindEventListeners();
